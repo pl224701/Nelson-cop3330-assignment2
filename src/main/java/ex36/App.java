c721141bd311734e6dbca0b9708521d462dbef3e
pl224701 <90616641+pl224701@users.noreply.github.com>
2021-09-28 01:17:59 -0400

    Update App.java

--- src/main/java/ex36/App.java
+++ src/main/java/ex36/App.java
@@ -9,14 +9,60 @@ import java.util.*;
 
 public class App
 {
-    public static void output(List<Integer> list, int elements)
+    public static float std(List<Integer> list,float average, int elements)
     {
         System.out.println(Arrays.toString(list.toArray()));
-        int total;
+        Integer[] numbers = new Integer[list.size()];
+        numbers=list.toArray(numbers);
+        float std=0;
         for(int i=0;i<elements;i++)
         {
-
+            std= (float) (std+Math.pow(average-numbers[i],2));
         }
+        std=std/(elements);
+        std=(float)Math.sqrt(std);
+        return std;
+    }
+    public static int max(List<Integer> list)
+    {
+        Integer[] numbers = new Integer[list.size()];
+        numbers=list.toArray(numbers);
+        int max=numbers[0];
+        for(int i=1;i<list.size();i++)
+        {
+            if(max<numbers[i])
+            {
+                max=numbers[i];
+            }
+        }
+        return max;
+    }
+    public static int min(List<Integer> list)
+    {
+        Integer[] numbers = new Integer[list.size()];
+        numbers=list.toArray(numbers);
+        int min=numbers[0];
+        for(int i=1;i<list.size();i++)
+        {
+            if(min>numbers[i])
+            {
+                min=numbers[i];
+            }
+        }
+        return min;
+    }
+    public static float average(List<Integer> list, int elements)
+    {
+        System.out.println(Arrays.toString(list.toArray()));
+        Integer[] numbers = new Integer[list.size()];
+        numbers=list.toArray(numbers);
+        float total=0;
+        for(int i=0;i<elements;i++)
+        {
+            total=total+numbers[i];
+        }
+        float average = total/elements;
+        return average;
     }
     public static void main(String[] args)
     {
@@ -28,13 +74,24 @@ public class App
         {
             System.out.print("Enter a number: ");
             str=sc.nextLine();
-            if(!str.equals("done"))
+            if(!str.matches("[0-9]+"))
+            {
+                System.out.println("Invald input");
+            }
+            if(!str.equals("done")&&str.matches("[0-9]+"))
             {
                 int number = Integer.parseInt(str);
                 list.add(number);
                 elements++;
             }
         }
-        output(list,elements);
+        float average = average(list,elements);
+        System.out.printf("The average is %.1f\n",average);
+        int min = min(list);
+        System.out.printf("The minimum is %d\n",min);
+        int max = max(list);
+        System.out.printf("The maximum is %d\n",max);
+        float std = std(list,average,elements);
+        System.out.printf("The standard deviation is %.2f\n",std);
     }
 }
